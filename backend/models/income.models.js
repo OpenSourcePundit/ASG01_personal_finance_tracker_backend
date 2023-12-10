bdbe81ca4f5844829c9e48b74bb0f4adf576530f
Shashank Kumar <shashank00098@gmail.com>
2023-12-10 11:37:05 +0530

    Models created

---
+++ backend/models/income.models.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const incomeSchema = new mongoose.Schema({
+    description:{
+        type:String,
+        required:true
+    },
+    amount:{
+        type:Number,
+        default:0,
+    },
+    date:{
+        type:Date,
+        default:Date.now(),
+    },
+    category:{
+        type:String,
+        enum:["Salary","Wages","Freelance","Dividend","Interest","Rent-Income","Profit","Capital Gains","Bonus","Gifts","None"],
+        default:"None"
+    }
+},{
+    timestamps:true
+})
+
+const Income = mongoose.model("Income",incomeSchema);
+
+export default Income
